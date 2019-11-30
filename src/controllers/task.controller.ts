26d33d9a7a025fd853da3d2e464d6cd0407f44ea
Ionut Traistaru <ionut.m.traistaru@gmail.com>
2019-11-30 14:39:05 +0000

    create error handler

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,6 +1,7 @@
 import { Task } from '../schemas/task.schema';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import asyncWrapper from '../utils/async-wrapper';
+import Err from '../utils/error-handler';
 
 // Create a new task and store it into DB
 export const create = asyncWrapper(
@@ -20,30 +21,30 @@ export const findAll = asyncWrapper(
   async (req: Request, res: Response): Promise<Response> => {
     const tasks = await Task.find({ creator: (req as any).userData.userId });
 
-    return res.status(200).json({ message: 'Loading tasks successfully', tasks });
+    return res.status(200).json({ message: 'Loading tasks successfully', results: tasks.length, tasks });
   }
 );
 
 // Get a single task from DB
 export const findOne = asyncWrapper(
-  async (req: Request, res: Response): Promise<Response> => {
+  async (req: Request, res: Response, next: NextFunction): Promise<void | Response> => {
     const task = await Task.findById(req.params.id);
 
     if (!task) {
-      return res.status(404).json({ message: 'Task not found' });
+      return next(new Err('Task not found', 404));
     }
 
-    res.status(200).json(task);
+    return res.status(200).json(task);
   }
 );
 
 // Update task on DB
 export const updateOne = asyncWrapper(
-  async (req: Request, res: Response): Promise<Response> => {
+  async (req: Request, res: Response, next: NextFunction): Promise<void | Response> => {
     const task = await Task.findByIdAndUpdate(req.params.id, req.body, { runValidators: true, new: true });
 
     if (!task) {
-      return res.status(404).json({ message: 'Task not found' });
+      return next(new Err('Task not found', 404));
     }
 
     res.status(200).json({ message: 'Task updated', task });
@@ -52,11 +53,11 @@ export const updateOne = asyncWrapper(
 
 // Delete task from DB
 export const deleteOne = asyncWrapper(
-  async (req: Request, res: Response): Promise<Response> => {
+  async (req: Request, res: Response, next: NextFunction): Promise<void | Response> => {
     const task = await Task.findByIdAndDelete(req.params.id);
 
     if (!task) {
-      return res.status(404).json({ message: 'Task not found' });
+      return next(new Err('Task not found', 404));
     }
 
     res.status(204).json({ message: 'Task deleted' });
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,8 @@
 import jwt from 'jsonwebtoken';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { User } from '../schemas/user.schema';
 import asyncWrapper from '../utils/async-wrapper';
+import Err from '../utils/error-handler';
 
 // Create user
 export const signup = asyncWrapper(
@@ -20,13 +21,13 @@ export const signup = asyncWrapper(
 
 // Log in user
 export const login = asyncWrapper(
-  async (req: Request, res: Response): Promise<Response> => {
+  async (req: Request, res: Response, next: NextFunction): Promise<void | Response> => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email }).select('+password');
 
     if (!user || !user.checkPassword(password, user.password)) {
-      return res.status(401).json({ message: 'Authentication failed! Email or password is incorrect.' });
+      return next(new Err('Authentication failed! Email or password is incorrect.', 401));
     }
 
     const token = jwt.sign({ email: user.email, userId: user._id }, process.env.JWT_SECRET, { expiresIn: '8h' });
